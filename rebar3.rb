c45cf4d92e58ba5d30b13ad578b03091ff29ac9d
BrewTestBot <brew-test-bot@googlegroups.com>
2016-11-05 08:13:18 +0000

    rebar3: update 3.3.2 bottle.

--- rebar3.rb
+++ rebar3.rb
@@ -8,10 +8,9 @@ class Rebar3 < Formula
 
   bottle do
     cellar :any_skip_relocation
-    rebuild 1
-    sha256 "f26c0fe6f6436b1459eb4a41b35ad665b9796c94830e1aff111cd485149c0b23" => :sierra
-    sha256 "13de8253f868a9f336711be8f94315a99b6f775243c772dc5f702687e1b6eddd" => :el_capitan
-    sha256 "7154d9627c4ccb295f95adb6eb3626514b2737e92b7ff9412169f72bf6a5068d" => :yosemite
+    sha256 "309100a2b267bad724992dbae9714d0ee4eb530b974809f13e0375f49320d5ca" => :sierra
+    sha256 "8c195c6cc0b45e114d6fb84b36644687650a2d63d748b802821a133094e16ccc" => :el_capitan
+    sha256 "3a9d5f37f96d3ca413bf5a63703f6aeb7bca41cc6f0cca03d7b3cec74e61593e" => :yosemite
   end
 
   depends_on "erlang"
