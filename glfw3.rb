659dc652f5e10bd37d3c27b02426fed642a16ed8
Harish Bajaj <henrymuppet@users.noreply.github.com>
2016-12-02 02:04:08 -0800

    glfw3: add HEAD install option (#1467)

--- glfw3.rb
+++ glfw3.rb
@@ -4,6 +4,8 @@ class Glfw3 < Formula
   url "https://github.com/glfw/glfw/archive/3.2.1.tar.gz"
   sha256 "e10f0de1384d75e6fc210c53e91843f6110d6c4f3afbfb588130713c2f9d8fe8"
 
+  head "https://github.com/glfw/glfw.git"
+
   bottle do
     cellar :any
     sha256 "299adbffdf82bc6040b126d317cd90cd6569fe0e4e16ce64fdc06e926797f805" => :el_capitan
