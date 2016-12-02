e26702a783c40eecc2a59040e49d4000435cdfe4
Alan Orth <alan.orth@gmail.com>
2016-12-02 12:04:22 +0200

    tomcat7 7.0.73 (#1468)

--- tomcat7.rb
+++ tomcat7.rb
@@ -1,19 +1,19 @@
 class Tomcat7 < Formula
   desc "Implementation of Java Servlet and JavaServer Pages"
   homepage "https://tomcat.apache.org/"
-  url "https://www.apache.org/dyn/closer.cgi?path=tomcat/tomcat-7/v7.0.72/bin/apache-tomcat-7.0.72.tar.gz"
-  sha256 "779c7bba49a9ed3cafc47a87c8f331d2068f2545105fdd1c1076e0357ebda5db"
+  url "https://www.apache.org/dyn/closer.cgi?path=tomcat/tomcat-7/v7.0.73/bin/apache-tomcat-7.0.73.tar.gz"
+  sha256 "0e814d6ad7d5b90e29c79887137420d3bc413540f9faa60d98f11e6c8a8fea85"
 
   bottle :unneeded
 
-  conflicts_with "tomcat", :because => "Differing versions of same formula"
-
   option "with-fulldocs", "Install full documentation locally"
 
+  conflicts_with "tomcat", :because => "Differing versions of same formula"
+
   resource "fulldocs" do
-    url "https://www.apache.org/dyn/closer.cgi?path=/tomcat/tomcat-7/v7.0.72/bin/apache-tomcat-7.0.72-fulldocs.tar.gz"
-    version "7.0.72"
-    sha256 "bea99925baa6ac0e940a231655b125f0f1ccdc43c9150ebc3984e8fbf7305975"
+    url "https://www.apache.org/dyn/closer.cgi?path=/tomcat/tomcat-7/v7.0.73/bin/apache-tomcat-7.0.73-fulldocs.tar.gz"
+    version "7.0.73"
+    sha256 "ef1395034fd59cea38aaeee853e2630a1fb6e59c3fd0f398c2ffbe7893fd450b"
   end
 
   # Keep log folders
@@ -29,5 +29,22 @@ class Tomcat7 < Formula
     bin.install_symlink "#{libexec}/bin/catalina.sh" => "catalina"
 
     (share/"fulldocs").install resource("fulldocs") if build.with? "fulldocs"
+  end
+
+  test do
+    ENV["CATALINA_BASE"] = testpath
+    cp_r Dir["#{libexec}/*"], testpath
+    rm Dir["#{libexec}/logs/*"]
+
+    pid = fork do
+      exec bin/"catalina", "start"
+    end
+    sleep 3
+    begin
+      system bin/"catalina", "stop"
+    ensure
+      Process.wait pid
+    end
+    File.exist? testpath/"logs/catalina.out"
   end
 end
